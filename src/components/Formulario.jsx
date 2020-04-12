f95653ff4dc97e5f672635f938f93ef95d6bd8b7
Miguel Briones <migg.briones@gmail.com>
2020-04-11 23:52:59 -0500

    Se agregan los elementos del Formulario.

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,8 +1,53 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const Formulario = () => {
     return ( 
-        <h1>Desde componente Formulario...</h1>
+        <Fragment>
+            <h2>Crear Cita</h2>
+            <form>
+                <label>Nombre de Mascotas</label>
+                <input
+                    type="text"
+                    name="mascota"
+                    className="u-full-width"
+                    placeholder="Nombre de Mascota"
+                />
+                
+                <label>Nombre de Dueño</label>
+                <input
+                    type="text"
+                    name="propietario"
+                    className="u-full-width"
+                    placeholder="Nombre dueño de la mascota"
+                />
+
+                <label>Fecha</label>
+                <input
+                    type="date"
+                    name="fecha"
+                    className="u-full-width"
+                />
+
+                <label>Hora</label>
+                <input
+                    type="time"
+                    name="hora"
+                    className="u-full-width"
+                />
+                
+                <label>Síntomas</label>
+                <textarea
+                    name="sintomas"
+                    className="u-full-width"
+                ></textarea>
+
+                <button
+                    type="submit"
+                    className="u-full-width button-primary"
+                >Agregar Cita
+                </button>
+            </form>
+        </Fragment>
      );
 }
  
