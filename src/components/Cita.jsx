e67c3fb88f04987867cbe7c86f289a67c8c2be63
Miguel Briones <migg.briones@gmail.com>
2020-04-12 02:03:52 -0500

    Se eliminan Citas del State y sólo se muestran las Citas previamente dadas de alta.

--- src/components/Cita.jsx
+++ src/components/Cita.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
-const Cita = ({cita}) => (
+const Cita = ({cita, eliminarCita}) => (
     <div className="cita">
-        <span>
-            <p>Mascota: {cita.mascota}</p>
-            <p>Dueño: {cita.propietario}</p>
-            <p>Fecha: {cita.fecha}</p>
-            <p>Hora: {cita.hora}</p>
-            <p>Sintomas: {cita.sintomas}</p>
-        </span>
+        <p>Mascota: <span> {cita.mascota} </span></p>
+        <p>Dueño: <span> {cita.propietario} </span></p>
+        <p>Fecha: <span> {cita.fecha} </span></p>
+        <p>Hora: <span> {cita.hora} </span></p>
+        <p>Sintomas: <span> {cita.sintomas} </span></p>
+    <button
+        className="button eliminar u-full-width"
+        onClick={ () => eliminarCita(cita.id) }
+    >Eliminar &times;
+    </button>
     </div> 
 );
  
