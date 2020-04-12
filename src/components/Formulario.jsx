4395e7e24047e9fe22b6de2c74a56888015300ae
Miguel Briones <migg.briones@gmail.com>
2020-04-12 00:59:49 -0500

    Se agrega validación del Formulario para requerir elementos necesarios en el Submit.

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -9,6 +9,8 @@ const Formulario = () => {
         hora: '',
         sintomas: ''
     });
+    const [error, actualizarErrror] = useState(false);
+
 
     // Función que se ejecuta cada que el usuario escribe en un input
     const actualizarState = e => {
@@ -22,14 +24,51 @@ const Formulario = () => {
 
     }
 
+
+
     // Extraer los valores
     const {mascota, propietario, fecha, hora, sintomas } = cita;
 
-    
+    // Cuando el usuario presionar agregar cita
+    const submitCita = e => {
+        e.preventDefault();
+        
+        console.log(cita);
+
+        // Validar
+        if(mascota.trim() === '' || propietario.trim() === '' || 
+            fecha.trim() === '' || hora.trim() === '' ||
+            sintomas.trim() === '' ) {
+            
+            actualizarErrror(true);
+            return;
+        }
+
+
+
+
+
+        // Asignar ID
+
+        // Crear la cita
+
+        // Reiniciar el form
+    }
+ 
+
     return ( 
         <Fragment>
             <h2>Crear Cita</h2>
-            <form>
+
+            {
+            error ? <p className="alerta-error">Todos los campos son obligatorios.</p>
+            :null
+            }
+
+
+            <form
+                onSubmit={submitCita}
+            >
                 <label>Nombre de Mascotas</label>
                 <input
                     type="text"
